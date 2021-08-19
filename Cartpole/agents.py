Fix policy gradient REINFORCE algorithm

Cartpole/agents.py
@@ -87,54 +87,42 @@ class QLearningAgent:
 class PolicyGradientAgent:
     """ Agent that learns using Policy Gradient Method """
 
-    def __init__(self, env, policy_model=None, value_model=None, optimizer=None, lr=1e-3):
+    def __init__(self, model, gamma=0.99, optimizer=None, lr=1e-3):
         """ Constructor 
         
-        :param env: Environment object with same interface as OpenAI gym CartPole
-        :param policy_model: Function approximation model that gives pi(a|s)
-        :param value_model: Function apporximation model that gives Q(s, a)
+        :param model: A function approximation model for pi(a|s)
+        :param gamma: Discount factor hyperparameter
+        :param optimizer: A pytorch optimizer
+        :param lr: Learning rate for gradient descent
         """
-
-        # Attach or create policy model pi(a|s)
-        if policy_model:
-            self.policy_model = policy_model
-        else:
-            self.policy_model = PolicyModel(env)
-
-        # Attach or create value model Q(s, a)
-        if value_model:
-            self.value_model = value_model
-        else:
-            self.value_model = RBFRegressionModel(env)
+        self.model = model
+        self.gamma = gamma
 
         # Attach or create the optimizer
         if optimizer:
             self.optim = optimizer
         else:
-            self.optim = optim.Adam(self.policy_model.parameters(), lr=lr)
+            self.optim = optim.Adam(self.model.parameters(), lr=lr)
 
-    def play(self, env, gamma=0.9, max_steps=2000, render=False):
+    def play(self, env, max_steps=2000, render=False):
         """ 
         Play one episode of CartPole. This is a Monte Carlo Method.
         
         :param env: Environment object with same interface as OpenAI gym CartPole
-        :param gamma: Discount rate for future rewards
         :param max_steps: Maximum iterations for an episode. The original 200 max steps of OpenAI Gym is too small
         :param render: Flag for rendering if needed
         """
 
         # Initialize metrics and state for episode
         s = env.reset()
-        done = False
-        # total_rewards = 0
-        step = 0
 
         # Keep track of (s, a, r) for an episode
         states = []
         actions = []
         rewards = []
 
-        while not done and step < max_steps:
+        # while not done and step < max_steps:
+        for _ in range(max_steps):
             action = self.select_action(s)
             s_prime, reward, done, _ = env.step(action)
 
@@ -150,57 +138,55 @@ class PolicyGradientAgent:
             # Don't forget to update state
             # Or agent won't learn
             s = s_prime
-            step += 1
+
+            if done:
+                break
 
         # Collect the returns and difference from value model
         G = 0
         returns = []
         for r in rewards[::-1]:
-            G = r + gamma * G
+            G = r + self.gamma * G
             returns.append(G)
-        returns.reverse()
-        
-        loss = self.update_policy(states, actions, returns)
 
-        return loss, sum(rewards)
+        returns.reverse()
+
+        return states, actions, rewards, returns
 
 
     def select_action(self, obs):
         """ Select the action to take based on the policy pi(a|s) """
-        action_logits = self.policy_model(obs)
-
-        with torch.no_grad():
-            action_probs = nn.functional.softmax(action_logits, -1)
-        
-        action_probs = action_probs.numpy()
-
+        action_probs, _ , _ = self.model(obs)
         return np.random.choice(len(action_probs), p=action_probs)
 
-    def update_policy(self, obs, actions, G):
+    def train(self, states, actions, rewards, returns):
         """
         Backpropagation update for policy model pi(a|s)
 
         :param states: States of an episode
-        :type states: numpy.ndarray
+        :type states: list
         :param actions: Actions of an episode
-        :type actions: numpy.ndarray
-        :param G: Either the list of returns or the list of advantages (G - V(s))
-        :type G: numpy.ndarray (numpy.float64)
+        :type actions: list
+        :param returns: Either the list of returns or the list of advantages (G - V(s))
+        :type returns: list
         """
         # Setup tensors
-        action_tensors = nn.functional.one_hot(torch.as_tensor(actions))
-        G_tensors = torch.as_tensor(G, dtype=torch.float32)
+        states = torch.tensor(states, dtype=torch.float32)
+        actions = torch.tensor(actions)
+        returns = torch.tensor(returns, dtype=torch.float32)
 
         # Get trained policy distribution model pi(a|s)
-        action_logits = self.policy_model(obs)
-        action_logprob = (nn.functional.log_softmax(action_logits, -1) * action_tensors).sum(1)
+        _, action_probs, log_probs = self.model(states)
+
+        # Get the log probability of selected actions
+        log_probs_selected_actions = (log_probs * nn.functional.one_hot(actions)).sum(1)
 
         # Calculate loss
-        loss = -((action_logprob * G_tensors).mean())
+        loss = -(log_probs_selected_actions * returns).mean()
 
         # Optimize
         self.optim.zero_grad()
         loss.backward()
         self.optim.step()
 
-        return loss
+        return np.sum(rewards)

Cartpole/models.py
@@ -2,7 +2,7 @@
 A set of custom models for predicting a Q(s,a) value from a state s
 """
 import torch
-from torch import nn, optim
+from torch import nn
 import numpy as np
 from sklearn.pipeline import FeatureUnion
 from sklearn.preprocessing import StandardScaler
@@ -10,7 +10,6 @@ from sklearn.kernel_approximation import RBFSampler
 from sklearn.linear_model import SGDRegressor
 
 from pdb import set_trace as bp
-
 
 class CustomSGDRegressor:
     """
@@ -110,27 +109,17 @@ class RBFRegressionModel:
 class PolicyModel(nn.Module):
     """ A Pytorch model approximation of pi(a|s) for Policy Gradient Method """
 
-    def __init__(self, env):
+    def __init__(self, n_states, n_actions):
         """ Constructor 
         
-        :param env: Environment object with same interface as OpenAI gym CartPole
+        :param n_states: State/observation space of the environment
+        :param n_actions: Action space of the agent
         """
         super(PolicyModel, self).__init__()
-        
-        # Get sample state for dimensions
-        sample_state = env.observation_space.sample()
-        input_dim = sample_state.shape
-
-        if len(input_dim) > 1:
-            # Raise error because state dimension is wrong
-            raise ValueError("Sample state space dimension expected to be (4,) but got {} instead".format(input_dim))
-
-        # Get size of action space
-        n_actions = env.action_space.n
 
         # Neural Network
         self.network = nn.Sequential(
-            nn.Linear(input_dim[0], 32),
+            nn.Linear(n_states, 32),
             nn.ReLU(),
             nn.Linear(32, 32),
             nn.ReLU(),
@@ -141,14 +130,23 @@ class PolicyModel(nn.Module):
         """ 
         Forward propagation for policy model pi(a|s)
         
-        :param s: Input state parameter
+        :param s: Input state of dim [batch_size, n_states]
         :type s: numpy.ndarray
 
-        :type return: torch.Tensor (torch.float32)
+        :returns:
+            - no_grad_logits - probabilities of each action without gradient (for action selection)
+            - logits - probabilities of each action with gradient (for backpropagation)
+            - log_probs - log probabilities of each action with gradient (for backpropagation)
         """
         input_tensor = torch.as_tensor(s, dtype=torch.float32)
-        logits = self.network(input_tensor)
-        return logits
+        x = self.network(input_tensor)
+
+        with torch.no_grad():
+            no_grad_logits = nn.functional.softmax(x, -1)
+
+        logits = nn.functional.softmax(x, -1)
+        log_prob = nn.functional.log_softmax(x, -1)
+        return no_grad_logits.numpy(), logits, log_prob
 
 
         

Simulation/CartPole.py
@@ -1,12 +1,14 @@
 """
 Experiments for CartPole
 """
+from Cartpole.models import PolicyModel
 import gym
 import sys
 import numpy as np
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 from Cartpole.agents import QLearningAgent, PolicyGradientAgent
+from Cartpole.models import PolicyModel
 from AgentUtils.plots import plot_running_avg
 
 sys.path.insert(0, "../Cartpole")
@@ -15,21 +17,33 @@ def main():
     env = gym.make("CartPole-v0")
     policy_gradient_learning(env)
 
-def policy_gradient_learning(env, N=1000):
+def policy_gradient_learning(env, N=10000):
+
+    # gym compatibility: unwrap TimeLimit
+    if hasattr(env, '_max_episode_steps'):
+        env = env.env
+    
     env.reset()
-    agent = PolicyGradientAgent(env)
-    total_rewards = np.empty(N)
+
+    n_actions = env.action_space.n
+    n_states = env.observation_space.shape[0]
+
+    model = PolicyModel(n_states, n_actions)
+    agent = PolicyGradientAgent(model)
+    total_rewards = []
     for n in tqdm(range(N)):
-        loss, total_reward = agent.play(env)
-        total_rewards[n] = total_reward
-        tqdm.write(" Episode {} gets total reward {} and loss {}.".format(n, total_reward, loss))
+        states, actions, rewards, returns = agent.play(env)
+        total_reward = agent.train(states, actions, rewards, returns)
+        total_rewards.append(total_reward)
+        if n%100 == 0:
+            tqdm.write(" Episode {} Reward: {}. Average Reward: {}".format(n, total_reward, np.mean(total_rewards[-100:])))
 
     # Sanity check on training
     plt.plot(total_rewards)
     plt.title("Rewards")
     plt.show()
 
-    plot_running_avg(total_rewards)
+    plot_running_avg(np.array(total_rewards))
 
      # Test if the agent is properly trained
     agent.play(env, render=True)